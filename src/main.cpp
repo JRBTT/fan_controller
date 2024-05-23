67231a3c9b16866f635e2d445456cbaa3e3a4747
JRBTT <jason.ray.buntting@gmail.com>
2024-05-24 00:39:58 +1000

    Input capture successfully works

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,13 @@
 
 volatile bool adcReady = false; 
 volatile int adcResult = 0;
+volatile float T1 = 0.0;
+volatile float T2 = 0.0;
+volatile float T_high = 0.0;
+volatile float T_low = 0.0;
+volatile float freq = 0.0;
+volatile uint32_t timerValue = 0;
+volatile bool periodCaptured = false;
 
 void setAdcbit(){
   // ADC initialization for reading the thermistor
@@ -30,6 +37,7 @@ int updateADC(){
   bitSet(ADCSRA, ADSC); // start conversion
 }
 
+
 ISR(ADC_vect) {
     // Handle the ADC conversion result
     adcResult = ADC;
@@ -38,20 +46,43 @@ ISR(ADC_vect) {
     // Do something with result
 }
 
+ISR(TIMER1_CAPT_vect) {
+  float capturedValue = ICR1;
+
+  if (!(TCCR1B & (1 << ICES1))) {
+    // Falling edge
+    T1 = capturedValue;
+    TCCR1B ^= (1 << ICES1); // Toggle the edge detection
+    T_low = T1 - T2;
+  } else {
+    // Rising edge
+    T2 = capturedValue;
+    TCCR1B ^= (1 << ICES1); // Toggle the edge detection
+    T_high = T2 - T1;
+  }
+}
+
 void setupPWM(){
   bitSet(DDRB, PB2);
   // Fast PWM with OCR1A as TOP
-  TCCR1A = (1 << WGM11) | (1 << WGM10);
-  TCCR1B = (1 << WGM13) | (1 << WGM12);
-  TCCR1A |= (1 << COM1B1); // non-inverting mode
-  OCR1A = 79; // 25kHz
-  TCCR1B |= (1 << CS11);
-  OCR1B = 31;
+  // TCCR1A = (1 << WGM11) | (1 << WGM10);
+  // TCCR1B = (1 << WGM13) | (1 << WGM12);
+  // TCCR1A |= (1 << COM1B1); // non-inverting mode
+  // OCR1A = 79; // 25kHz
+  // OCR1B = 40;
+
+  // Input Capture setup
+  TCCR1B |= (1 << ICES1); // Capture on rising edge
+  bitClear(DDRB, PB0); // Set ICP1 (PB0) as input
+  TIMSK1 |= (1 << ICIE1); // Enable Input Capture interrupt
+  TCCR1B = (1 << CS12); // prescaler 256 (62.5kHz) with overflow at1.04857s
+
 }
 
 void setup()
 {
   usart_init(MYUBRR); // 103-9600 bps; 8-115200
+  setupPWM();
   setAdcbit(); // set ADC5 as input
   sei(); // enable global interrupts
   updateADC();
@@ -63,18 +94,25 @@ void setup()
 int main()
 {
     setup();
-    setupPWM();
+    
     float temp;
 
     while(1)
     {
-        if (adcReady){
-            usart_tx_string(">Temperature: ");
-            temp = getTemperature(adcResult);
-            usart_tx_float(temp, 3, 2);
-            usart_transmit('\n');
-            adcReady = false;
-            updateADC();
-        }
+      // if (adcReady){
+      //     usart_tx_string(">Temperature: ");
+      //     temp = getTemperature(adcResult);
+      //     usart_tx_float(temp, 3, 2);
+      //     usart_transmit('\n');
+      //     adcReady = false;
+      //     updateADC();
+      // }
+      if (T_high + T_low > 0.0){
+        float time = (T_high + T_low) * 0.000016;
+        freq = 1 / time;
+        usart_tx_string(">duty_estimate: ");
+        usart_tx_float(freq, 6, 3);
+        usart_transmit('\n');
+      }
     }
 }
