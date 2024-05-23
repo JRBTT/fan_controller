e8cf0ac8ae9ec071db34d309671bc895bdc6ad04
JRBTT <jason.ray.buntting@gmail.com>
2024-05-24 01:58:50 +1000

    analog comparator done

--- src/main.cpp
+++ src/main.cpp
@@ -46,23 +46,39 @@ ISR(ADC_vect) {
     // Do something with result
 }
 
-ISR(TIMER1_CAPT_vect) {
-  float capturedValue = ICR1;
+// ISR(TIMER1_CAPT_vect) {
+//   float capturedValue = ICR1;
 
-  if (!(TCCR1B & (1 << ICES1))) {
+//   if (!(TCCR1B & (1 << ICES1))) {
+//     // Falling edge
+//     T1 = capturedValue;
+//     TCCR1B ^= (1 << ICES1); // Toggle the edge detection
+//     T_low = T1 - T2;
+//   } else {
+//     // Rising edge
+//     T2 = capturedValue;
+//     TCCR1B ^= (1 << ICES1); // Toggle the edge detection
+//     T_high = T2 - T1;
+//   }
+// }
+
+ISR(ANALOG_COMP_vect) {
+  float capturedValue = TCNT1;  // Capture the current value of Timer1
+
+  if (!(ACSR & (1 << ACIS0))) {
     // Falling edge
     T1 = capturedValue;
-    TCCR1B ^= (1 << ICES1); // Toggle the edge detection
+    ACSR ^= (1 << ACIS0); // Toggle the edge detection
     T_low = T1 - T2;
   } else {
     // Rising edge
     T2 = capturedValue;
-    TCCR1B ^= (1 << ICES1); // Toggle the edge detection
+    ACSR ^= (1 << ACIS0); // Toggle the edge detection
     T_high = T2 - T1;
   }
 }
 
-void setupPWM(){
+void setupTach(){
   bitSet(DDRB, PB2);
   // Fast PWM with OCR1A as TOP
   // TCCR1A = (1 << WGM11) | (1 << WGM10);
@@ -72,9 +88,18 @@ void setupPWM(){
   // OCR1B = 40;
 
   // Input Capture setup
-  TCCR1B |= (1 << ICES1); // Capture on rising edge
-  bitClear(DDRB, PB0); // Set ICP1 (PB0) as input
-  TIMSK1 |= (1 << ICIE1); // Enable Input Capture interrupt
+  // TCCR1B |= (1 << ICES1); // Capture on rising edge
+  // bitClear(DDRB, PB0); // Set ICP1 (PB0) as input
+  // TIMSK1 |= (1 << ICIE1); // Enable Input Capture interrupt
+
+  // Analog Comparator setup
+  bitClear(DDRD, PD7); // negative
+  bitClear(DDRD, PD6); // positive reference 3.3v
+
+  ACSR = (0 << ACD) | (1 << ACBG) | (1 << ACIS1) | (1 << ACIS0); // Bandgap reference, rising edge
+  ACSR |= (1 << ACIE); // Enable Analog Comparator interrupt
+
+
   TCCR1B = (1 << CS12); // prescaler 256 (62.5kHz) with overflow at1.04857s
 
 }
@@ -82,7 +107,7 @@ void setupPWM(){
 void setup()
 {
   usart_init(MYUBRR); // 103-9600 bps; 8-115200
-  setupPWM();
+  setupTach();
   setAdcbit(); // set ADC5 as input
   sei(); // enable global interrupts
   updateADC();
@@ -110,7 +135,7 @@ int main()
       if (T_high + T_low > 0.0){
         float time = (T_high + T_low) * 0.000016;
         freq = 1 / time;
-        usart_tx_string(">duty_estimate: ");
+        usart_tx_string(">fan freq: ");
         usart_tx_float(freq, 6, 3);
         usart_transmit('\n');
       }
